c3e316f3d556648fd8a665f18f40560993df3baa
NikChief <niki3000-90@mail.ru>
2022-03-29 00:08:12 +0500

    Ready

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const mascot = require('./app/models/mascot');
 // Фреймворк веб-приложений.
 const express = require('express');
 const morgan = require('morgan');
@@ -29,6 +30,37 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index', req.query);
 });
+
+//мой код
+app.get('/', (req, res) => {
+  const { signText } = req.query;
+  res.render('index', { signText });
+});
+
+// app.get('/cheers', (req, res) => {
+//   res.render('/', { signText });
+// });
+
+// //мой код 2
+// app.post('/cheers', (req, res) => {
+//   const signText = req.body.cheer_name;
+//   // console.log(typeof req.body.cheer_name);
+//   // res.render('index', { signText: x, secondVar: '2' });
+//   res.render('index', { signText });
+// });
+app.post('/cheers', (req, res) => {
+  const signText = req.body.cheer_name;
+  let searchedKey = Object.keys(mascot).find((el)=>el===signText);
+  let result;
+  if (searchedKey === undefined) { result = '( ╯°□°)╯'} else {result = mascot[searchedKey]
+};
+  // console.log(result);
+  // console.log(typeof req.body.cheer_name);
+  // res.render('index', { signText: x, secondVar: '2' });
+  res.render('index', { signText: result });
+  res.redirect(`http://localhost:3000?signText=${result}`);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
---
+++ app/models/mascot.js
@@ -0,0 +1,8 @@
+const mascot = {
+  'RED HOT': 'H-O-T!',
+  'DO IT AGAIN': 'Go, Fight, Win',
+  '2 BITS': 'Holler!',
+  'STOMP YOUR FEET': 'STOMP!',
+}
+
+module.exports = mascot;
